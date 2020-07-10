57da22237605bd0a2ea95252d9886e70802b4aa3
loomou <273226981@qq.com>
2020-07-11 01:03:44 +0800

    实现翻页功能

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -16,6 +16,19 @@
       ...mapGetters(['fileName'])
     },
     methods: {
+      prevPage() {
+        if (this.rendition) {
+          this.rendition.prev()
+        }
+      },
+      nextPage() {
+        if (this.rendition) {
+          this.rendition.next()
+        }
+      },
+      toggleTitleAndMenu() {
+
+      },
       initEpub() {
         const url = 'http://192.168.0.135:8081/epub/' + this.fileName + '.epub';
         this.book = new Epub(url);
@@ -25,6 +38,25 @@
           method: 'default'
         });
         this.rendition.display();
+        this.rendition.on('touchstart', event => {
+          this.touchStartX = event.changedTouches[0].clientX;
+          this.touchStartTime = event.timeStamp;
+          console.log(event);
+        });
+        this.rendition.on('touchend', event => {
+          const offsetX = event.changedTouches[0].clientX - this.touchStartX;
+          const time = event.timeStamp - this.touchStartTime;
+          console.log(offsetX, time);
+          if (time < 500 && offsetX > 40) {
+            this.prevPage();
+          } else if (time < 500 && offsetX < -40) {
+            this.nextPage();
+          } else {
+            this.toggleTitleAndMenu();
+          }
+          event.preventDefault();
+          event.stopPropagation();
+        });
       }
     },
     mounted() {
