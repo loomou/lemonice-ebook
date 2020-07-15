27d787024b822515addc6e6fe39e60d6020bb19a
loomou <273226981@qq.com>
2020-07-16 01:05:58 +0800

    书签手势-下拉状态管理

--- src/components/ebook/EbookBookmark.vue
+++ src/components/ebook/EbookBookmark.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ebook-bookmark">
+  <div class="ebook-bookmark" ref="bookmark">
     <div class="ebook-bookmark-text-wrapper">
       <div class="ebook-bookmark-down-wrapper">
         <span class="icon-down"></span>
@@ -7,23 +7,51 @@
       <div class="ebook-bookmark-text">{{text}}</div>
     </div>
     <div class="ebook-bookmark-icon-wrapper">
-      <bookmark :color="'red'" :width="15" :height="35"></bookmark>
+      <bookmark :color="color" :width="15" :height="35"></bookmark>
     </div>
   </div>
 </template>
 
 <script>
   import Bookmark from "../common/Bookmark";
+  import {ebookMixin} from "../../utils/mixin";
+  import {realPx} from "../../utils/utils";
+
+  const BLUE = '#346cbc';
+  const WHITE = '#fff';
 
   export default {
     name: "EbookBookmark",
+    mixins: [ebookMixin],
     components: {
       Bookmark,
     },
     data() {
       return {
-        text: this.$t('book.pulldownAddMark')
+        text: '',
+        color: WHITE,
       };
+    },
+    computed: {
+      height() {
+        return realPx(35);
+      },
+      threshold() {
+        return realPx(55);
+      }
+    },
+    watch: {
+      offsetY(v) {
+        if (v >= this.height && v <= this.threshold) {
+          this.$refs.bookmark.style.top = `${-v}px`;
+          this.text = this.$t('book.pulldownAddMark');
+          this.color = WHITE;
+        } else if (v >= this.threshold) {
+          this.$refs.bookmark.style.top = `${-v}px`;
+          this.text = this.$t('book.releaseAddMark');
+          this.color = BLUE;
+        }
+      }
     }
   };
 </script>
