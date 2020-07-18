0735c55127cb2e37f6ab596fa05d0a57456dca4d
loomou <273226981@qq.com>
2020-07-18 23:45:32 +0800

    书城首页-实现卡片翻转动画

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -6,8 +6,12 @@
            :key="index"
            :style="{zIndex: item.zIndex}">
         <div class="flap-card-circle">
-          <div class="flap-card-semi-circle flap-card-semi-circle-left" :style="semiCircleStyle(item, 'left')"></div>
-          <div class="flap-card-semi-circle flap-card-semi-circle-right" :style="semiCircleStyle(item, 'right')"></div>
+          <div class="flap-card-semi-circle flap-card-semi-circle-left"
+               :style="semiCircleStyle(item, 'left')"
+               ref="left"></div>
+          <div class="flap-card-semi-circle flap-card-semi-circle-right"
+               :style="semiCircleStyle(item, 'right')"
+               ref="right"></div>
         </div>
       </div>
     </div>
@@ -26,7 +30,10 @@
     mixins: [storeHomeMixin],
     data() {
       return {
-        flapCardList
+        flapCardList,
+        front: 0,
+        back: 1,
+        intervalTime: 25,
       };
     },
     methods: {
@@ -40,6 +47,81 @@
           backgroundImage: dir === 'left' ? item.imgLeft : item.imgRight
         };
       },
+      rotate(index, type) {
+        const item = this.flapCardList[index];
+        let dom;
+        if (type === 'front') {
+          dom = this.$refs.right[index];
+        } else {
+          dom = this.$refs.left[index];
+        }
+        dom.style.transform = `rotateY(${item.rotateDegree}deg)`;
+        dom.style.backgroundColor = `rgb(${item.r}, ${item._g}, ${item.b})`;
+      },
+      flapCardRotate() {
+        const frontFlapCard = this.flapCardList[this.front];
+        const backFlapCard = this.flapCardList[this.back];
+        frontFlapCard.rotateDegree += 10;
+        frontFlapCard._g -= 5;
+        backFlapCard.rotateDegree -= 10;
+        if (backFlapCard.rotateDegree < 90) {
+          backFlapCard._g += 5;
+        }
+        if (frontFlapCard.rotateDegree === 90 && backFlapCard.rotateDegree === 90) {
+          backFlapCard.zIndex += 2;
+        }
+        this.rotate(this.front, 'front');
+        this.rotate(this.back, 'back');
+        if (frontFlapCard.rotateDegree === 180 && backFlapCard.rotateDegree === 0) {
+          this.next();
+        }
+      },
+      next() {
+        const frontFlapCard = this.flapCardList[this.front];
+        const backFlapCard = this.flapCardList[this.back];
+        frontFlapCard.rotateDegree = 0;
+        backFlapCard.rotateDegree = 0;
+        frontFlapCard._g = frontFlapCard.g;
+        backFlapCard._g = backFlapCard.g;
+        this.rotate(this.front, 'front');
+        this.rotate(this.back, 'back');
+        this.front++;
+        this.back++;
+        const len = this.flapCardList.length;
+        if (this.front >= len) {
+          this.front = 0;
+        }
+        if (this.back >= len) {
+          this.back = 0;
+        }
+        // 动态设置zIndex
+        // 100 -> 96
+        // 99 -> 100
+        // 98 -> 99
+        // 97 -> 98
+        // 96 -> 97
+        // (0 - 1 + 5) % 5 = 4
+        // (1 - 1 + 5) % 5 = 0
+        this.flapCardList.forEach((item, index) => {
+          item.zIndex = 100 - ((index - this.front + len) % len);
+        });
+        this.prepare();
+      },
+      prepare() {
+        const backFlapCard = this.flapCardList[this.back];
+        backFlapCard.rotateDegree = 180;
+        backFlapCard._g = backFlapCard.g - 5 * 9;
+        this.rotate(this.back, 'back');
+      },
+      startFlapCardAnimation() {
+        this.prepare();
+        setInterval(() => {
+          this.flapCardRotate();
+        }, this.intervalTime);
+      },
+    },
+    mounted() {
+      this.startFlapCardAnimation();
     }
   };
 </script>
@@ -77,16 +159,19 @@
             width: 50%;
             height: 100%;
             background-repeat: no-repeat;
+            backface-visibility: hidden;
           }
 
           .flap-card-semi-circle-left {
             border-radius: px2rem(24) 0 0 px2rem(24);
             background-position: center right;
+            transform-origin: right;
           }
 
           .flap-card-semi-circle-right {
             border-radius: 0 px2rem(24) px2rem(24) 0;
             background-position: center left;
+            transform-origin: left;
           }
         }
       }
