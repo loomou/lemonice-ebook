4b74b7bf9eca30fbc7715e4bc284c93430195637
loomou <273226981@qq.com>
2020-07-20 01:25:08 +0800

    书架页面-图书列表-组件设计、动态组件和布局

--- src/components/shelf/ShelfItem.vue
+++ src/components/shelf/ShelfItem.vue
@@ -1,13 +1,42 @@
 <template>
-
+  <div class="shelf-item">
+    <component :is="item"></component>
+  </div>
 </template>
 
 <script>
+  import {storeShelfMixin} from "../../utils/mixin";
+  import ShelfBook from './ShelfItemBook';
+  import ShelfCategory from './ShelfItemCategory';
+  import ShelfAdd from './ShelfItemAdd';
+
   export default {
-    name: "ShelfItem"
+    name: "ShelfItem",
+    mixins: [storeShelfMixin],
+    data() {
+      return {
+        book: ShelfBook,
+        category: ShelfCategory,
+        add: ShelfAdd,
+      };
+    },
+    props: {
+      data: Object
+    },
+    computed: {
+      item() {
+        return this.data.type === 1 ? this.book : (this.data.type === 2 ? this.category : this.add);
+      }
+    }
   };
 </script>
 
 <style lang="scss" scoped>
+  @import "../../assets/styles/global";
 
+  .shelf-item {
+    width: 100%;
+    height: 100%;
+    background: #346cb9;
+  }
 </style>
--- src/components/shelf/ShelfList.vue
+++ src/components/shelf/ShelfList.vue
@@ -1,34 +1,25 @@
 <template>
   <div class="shelf-list">
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
-    <div>aaaaa</div>
+    <div class="shelf-list-item-wrapper" v-for="item in shelfList" :key="item.id">
+      <shelf-item :data="item" :style="{height: itemHeight}"></shelf-item>
+    </div>
   </div>
 </template>
 
 <script>
+  import {storeShelfMixin} from "../../utils/mixin";
+  import ShelfItem from "./ShelfItem";
+  import {realPx} from "../../utils/utils";
+
   export default {
-    name: "ShelfList"
+    name: "ShelfList",
+    components: {ShelfItem},
+    mixins: [storeShelfMixin],
+    computed: {
+      itemHeight() {
+        return ((window.innerWidth - realPx(120)) / 3) / 250 * 350 + 'px';
+      }
+    }
   };
 </script>
 
@@ -40,5 +31,17 @@
     top: px2rem(94);
     left: 0;
     z-index: 100;
+    display: flex;
+    flex-flow: row wrap;
+    width: 100%;
+    padding: 0 px2rem(15);
+    box-sizing: border-box;
+
+    .shelf-list-item-wrapper {
+      flex: 0 0 33.33%;
+      width: 33.33%;
+      padding: px2rem(15);
+      box-sizing: border-box;
+    }
   }
 </style>
