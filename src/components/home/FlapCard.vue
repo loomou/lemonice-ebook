5b72b7e28c997d104b0354d8608ec10bb1e1d20d
loomou <273226981@qq.com>
2020-07-19 18:07:08 +0800

    书城首页-推荐图书布局及动画、首页布局实现

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flap-card-wrapper" v-show="flapCardVisible">
-    <div class="flap-card-bg" :class="{'animation': runFlapCardAnimation}">
+    <div class="flap-card-bg"
+         :class="{'animation': runFlapCardAnimation}"
+         v-show="runFlapCardAnimation">
       <div class="flap-card"
            v-for="(item, index) in flapCardList"
            :key="index"
@@ -21,6 +23,19 @@
              :key="item"></div>
       </div>
     </div>
+    <div class="book-card" :class="{'animation': runBookCardAnimation}" v-show="runBookCardAnimation">
+      <div class="book-card-wrapper">
+        <div class="img-wrapper">
+          <img class="img" :src="data ? data.cover : ''">
+        </div>
+        <div class="content-wrapper">
+          <div class="content-title">{{data ? data.title : ''}}</div>
+          <div class="content-author sub-title-medium">{{data ? data.author : ''}}</div>
+          <div class="content-category">{{categoryText()}}</div>
+        </div>
+        <div class="read-btn" @click.stop="showBookDetail(data)">{{$t('home.readNow')}}</div>
+      </div>
+    </div>
     <div class="close-btn-wrapper" @click="close">
       <div class="icon-close"></div>
     </div>
@@ -29,7 +44,7 @@
 
 <script>
   import {storeHomeMixin} from "../../utils/mixin";
-  import {flapCardList} from "../../utils/store";
+  import {flapCardList, categoryText} from "../../utils/store";
 
   export default {
     name: "FlapCard",
@@ -46,6 +61,7 @@
         runFlapCardAnimation: false,
         pointList: null,
         runPointAnimation: false,
+        runBookCardAnimation: false,
       };
     },
     methods: {
@@ -136,16 +152,15 @@
           this.rotate(index, 'front');
           this.rotate(index, 'back');
         });
+        this.runBookCardAnimation = false;
+        this.runFlapCardAnimation = false;
+        this.runPointAnimation = false;
       },
       startFlapCardAnimation() {
         this.prepare();
         this.task = setInterval(() => {
           this.flapCardRotate();
         }, this.intervalTime);
-        setTimeout(() => {
-          this.runFlapCardAnimation = false;
-          this.stopAnimation();
-        }, 2500)
       },
       startPointAnimation() {
         this.runPointAnimation = true;
@@ -157,15 +172,33 @@
         if (this.task) {
           clearInterval(this.task);
         }
+        if (this.timeout) {
+          clearInterval(this.timeout);
+        }
+        if (this.timeout2) {
+          clearInterval(this.timeout2);
+        }
         this.reset();
       },
       runAnimation() {
         this.runFlapCardAnimation = true;
-        setTimeout(() => {
+        this.timeout = setTimeout(() => {
           this.startFlapCardAnimation();
           this.startPointAnimation();
         }, 300);
+        this.timeout2 = setTimeout(() => {
+          this.runFlapCardAnimation = false;
+          this.stopAnimation();
+          this.runBookCardAnimation = true;
+        }, 2500);
       },
+      categoryText() {
+        if (this.data) {
+          categoryText(this.data.category, this);
+        } else {
+          return '';
+        }
+      }
     },
     watch: {
       flapCardVisible(v) {
@@ -274,6 +307,88 @@
               }
             }
           }
+        }
+      }
+    }
+
+    .book-card {
+      position: relative;
+      width: 65%;
+      max-width: px2rem(400);
+      box-sizing: border-box;
+      border-radius: px2rem(15);
+      background: white;
+
+      &.animation {
+        animation: scale .3s ease-in both;
+        @keyframes scale {
+          0% {
+            transform: scale(0);
+            opacity: 0;
+          }
+          100% {
+            transform: scale(1);
+            opacity: 1;
+          }
+        }
+      }
+
+      .book-card-wrapper {
+        width: 100%;
+        height: 100%;
+        margin-bottom: px2rem(30);
+        @include columnTop;
+
+        .img-wrapper {
+          width: 100%;
+          margin-top: px2rem(20);
+          @include center;
+
+          .img {
+            width: px2rem(90);
+            height: px2rem(130);
+          }
+        }
+
+        .content-wrapper {
+          padding: 0 px2rem(20);
+          margin-top: px2rem(20);
+
+          .content-title {
+            color: #333;
+            font-weight: bold;
+            font-size: px2rem(18);
+            line-height: px2rem(20);
+            max-height: px2rem(40);
+            text-align: center;
+            @include ellipsis2(2)
+          }
+
+          .content-author {
+            margin-top: px2rem(10);
+            text-align: center;
+          }
+
+          .content-category {
+            color: #999;
+            font-size: px2rem(14);
+            margin-top: px2rem(10);
+            text-align: center;
+          }
+        }
+
+        .read-btn {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          z-index: 1100;
+          width: 100%;
+          border-radius: 0 0 px2rem(15) px2rem(15);
+          padding: px2rem(15) 0;
+          text-align: center;
+          color: white;
+          font-size: px2rem(14);
+          background: $color-blue;
         }
       }
     }
