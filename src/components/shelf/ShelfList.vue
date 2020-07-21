cc7c97c78e0ea8fe2546cb9ebe0700e53b52ca69
loomou <273226981@qq.com>
2020-07-21 22:49:09 +0800

    书架页面-书架列表动画

--- src/components/shelf/ShelfList.vue
+++ src/components/shelf/ShelfList.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="shelf-list">
-    <div class="shelf-list-item-wrapper" v-for="item in shelfList" :key="item.id">
-      <shelf-item :data="item" :style="{height: itemHeight}"></shelf-item>
-      <div class="shelf-list-title-wrapper">
-        <span class="shelf-list-title title-small">{{item.title}}</span>
+    <transition-group name="list" tag="div" id="shelf-list">
+      <div class="shelf-list-item-wrapper" v-for="item in shelfList" :key="item.id">
+        <shelf-item :data="item" :style="{height: itemHeight}"></shelf-item>
+        <div class="shelf-list-title-wrapper">
+          <span class="shelf-list-title title-small">{{item.title}}</span>
+        </div>
       </div>
-    </div>
+    </transition-group>
   </div>
 </template>
 
@@ -34,20 +36,31 @@
     top: px2rem(94);
     left: 0;
     z-index: 100;
-    display: flex;
-    flex-flow: row wrap;
     width: 100%;
-    padding: 0 px2rem(15);
-    box-sizing: border-box;
 
-    .shelf-list-item-wrapper {
-      flex: 0 0 33.33%;
-      width: 33.33%;
-      padding: px2rem(15);
+    #shelf-list {
+      display: flex;
+      flex-flow: row wrap;
+      padding: 0 px2rem(15);
       box-sizing: border-box;
 
-      .shelf-list-title-wrapper {
-        margin-top: px2rem(10);
+      .shelf-list-item-wrapper {
+        flex: 0 0 33.33%;
+        width: 33.33%;
+        padding: px2rem(15);
+        box-sizing: border-box;
+
+        &.list-leave-active {
+        display: none;
+        }
+
+        &.list-move {
+          transition: transform .5s;
+        }
+
+        .shelf-list-title-wrapper {
+          margin-top: px2rem(10);
+        }
       }
     }
   }
